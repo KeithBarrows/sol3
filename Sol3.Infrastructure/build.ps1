3504203a8a2839fc453108d5d46fece0219011cf
Keith Barrows <keith.barrows@sol3.net>
2018-09-05 17:31:50 -0600

    another tweak

--- Sol3.Infrastructure/build.ps1
+++ Sol3.Infrastructure/build.ps1
@@ -1,6 +1,5 @@
 Write-Host "---==[ BEFORE BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"
 dotnet --version
-##Set-Location .\$env:project_name
 dotnet restore --verbosity normal
 
 Write-Host "---==[ BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"
@@ -9,4 +8,3 @@ dotnet build --verbosity normal
 Write-Host "---==[ AFTER BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"
 dotnet pack --no-build /p:PackageVersion=$env:APPVEYOR_BUILD_VERSION --verbosity normal
 nuget push .\bin\release\$env:project_name.$env:APPVEYOR_BUILD_VERSION.nupkg -ApiKey gnsob4ooc2th7pjtxx7yqflr -Source https://ci.appveyor.com/nuget/keithbarrows/api/v2/package
-##Set-Location ..
