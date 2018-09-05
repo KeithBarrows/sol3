8b43cae28041574d56f8e0b1f7ad8cf5518124bd
Keith Barrows <keith.barrows@sol3.net>
2018-09-05 13:31:50 -0600

    working on reading nugets

--- build.ps1
+++ build.ps1
@@ -1,24 +1,17 @@
-########################################################################################
-Write-Host "---==[ BEFORE BUILD" $env:project_name "SCRIPT ]==---"  
-Write-Host "Build Number:" $env:APPVEYOR_BUILD_NUMBER
-Write-Host "Build Version:" $env:APPVEYOR_BUILD_VERSION
+Write-Host "---==[ BEFORE BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"  
 dotnet --version
 Set-Location .\$env:project_name
-Get-ChildItem
-dotnet restore ###$env:project_name/$env:project_name.csproj
+dotnet restore
 Write-Host ""
-########################################################################################
-Write-Host "---==[ BUILD" $env:project_name "SCRIPT ]==---"  
-dotnet build ###$env:project_name/$env:project_name.csproj -c Release
+
+Write-Host "---==[ BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"  
+dotnet build
 Write-Host ""
-########################################################################################
-Write-Host "---==[ AFTER BUILD" $env:project_name "SCRIPT ]==---"  
+
+Write-Host "---==[ AFTER BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"  
 if($env:project_name -ilike "sol3.infrastructure*")  
 {  
     dotnet pack --no-build /p:PackageVersion=$env:APPVEYOR_BUILD_VERSION
-    Set-Location .\bin\release
-    nuget push $env:project_name.$env:APPVEYOR_BUILD_VERSION.nupkg -ApiKey gnsob4ooc2th7pjtxx7yqflr -Source https://ci.appveyor.com/nuget/keithbarrows/api/v2/package
-    Set-Location ../..
+    nuget push .\bin\release\$env:project_name.$env:APPVEYOR_BUILD_VERSION.nupkg -ApiKey gnsob4ooc2th7pjtxx7yqflr -Source https://ci.appveyor.com/nuget/keithbarrows/api/v2/package
 }
 Set-Location ..
-########################################################################################
