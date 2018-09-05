b7c6ba78db746c8b0196362ead5132014543d087
Keith Barrows <keith.barrows@sol3.net>
2018-09-05 11:38:50 -0600

    tracking down version numbers

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 #########################################################################################
 Write-Host "---==[ BEFORE BUILD" $env:project_name "SCRIPT ]==---"  
+Write-Host "Build Number:" $env:APPVEYOR_BUILD_NUMBER
+Write-Host "Build Version:" $env:APPVEYOR_BUILD_VERSION
 dotnet --version
 Set-Location .\$env:project_name
 Get-ChildItem
@@ -13,9 +15,11 @@ Write-Host ""
 Write-Host "---==[ AFTER BUILD" $env:project_name "SCRIPT ]==---"  
 if($env:project_name -ilike "sol3.infrastructure*")  
 {  
-    dotnet pack --no-build
+    dotnet pack --no-build /p:PackageVersion=$env:APPVEYOR_BUILD_VERSION
+    Set-Location .\bin\release
     Get-ChildItem
-    Write-Host "nuget push $env:project_name.nuspec -ApiKey gnsob4ooc2th7pjtxx7yqflr -Source https://ci.appveyor.com/nuget/keithbarrows/api/v2/package"
+    Write-Host "nuget push $env:project_name.$env:APPVEYOR_BUILD_VERSION.nupkg -ApiKey gnsob4ooc2th7pjtxx7yqflr -Source https://ci.appveyor.com/nuget/keithbarrows/api/v2/package"
+    Set-Location ../..
 }
 Set-Location ..
 #########################################################################################
