cb2d6f9b5c2b284ea9e4e749020982ed7d454573
Keith Barrows <keith.barrows@sol3.net>
2018-09-06 12:22:49 -0600

    changed references to match new layout of code

--- Sol3.TestCli/Program.cs
+++ Sol3.TestCli/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Sol3.Infrastructure.Base.Configuration;
+using Sol3.Infrastructure.Configuration;
 using Sol3.Infrastructure.IoT.Weather.Models;
 
 namespace Sol3.TestCli
