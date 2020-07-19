cd69e6954bbcf044e1493803cb95ea45cbec6293
Keith <keith@sol3.net>
2020-07-19 16:01:21 -0400

    Removed unused/outdated files & projects

--- Sol3.Services.FileSystem/Models/TagEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sol3.Services.FileSystem.Models
-{
-    public class TagEntry
-    {
-        public TagType TagType { get; set; }
-        public string Tag { get; set; }
-        public string FriendlyTagType => TagType.ToString();
-    }
-}
--- Sol3.TestCli/build.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "---==[ BEFORE BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"
-dotnet --version
-dotnet restore --verbosity normal
-
-Write-Host "---==[ BUILD" $env:project_name $env:APPVEYOR_BUILD_VERSION "SCRIPT ]==---"
-dotnet build --verbosity normal
